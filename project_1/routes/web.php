1a8590607f1d7cc95ed0e97a598296fc2b9c6598
user <kamczyc.mateusz@gmail.com>
2023-09-27 13:55:11 +0200

    add new file

--- project_1/routes/web.php
+++ project_1/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Dflydev\DotAccessData\Data;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +30,109 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// WAŻNE Musisz dopisać to co jest podspodem zamiast kopiowac!
+// Route jest od klasy
+// ::get([nazwa którą trzeba wpisac po/ żeby wyswietlilo strone], function(){[Co ma wyświetlić po wpisaniu linka]})
+Route::get('city_old',function(){
+return 'Miasto';
+
+
+});
+Route::get('city',function(){
+
+return view('city');
+
+});
+Route::get('info',function(){
+    return view('info',['firstName' => 'Janusz', 'lastName' => 'Nowak', 'city' => 'Poznan']);
+});
+
+Route::get('info_age/{age}',function($age){
+    return view('info',['firstName' => 'Janusz', 'lastName' => 'Nowak', 'city' => 'Poznan','age' => $age]);
+});
+Route::get('pages/{page}',function($page){
+$pages = [
+    'about' => '2 słowa o mnie',
+    'contact' => 'contact@gmail.com',
+    'home' => 'Strona Domowa'
+];
+ return $pages[$page];
+
+});
+
+Route::redirect('miasto', 'city');
+//Route::redirect("/","city");
+
+Route::get('/address',function(){
+
+echo "Miasto";
+
+});
+Route::get('/address/{city}',function(string $city){
+
+    echo "Miasto : $city";
+    
+    });
+    Route::get('/address/{city}/{street}',function(string $city, string $street){
+
+        echo<<< Data
+         Miasto : $city <br>
+         Ulica : $street
+         <hr>
+        Data;
+        });
+Route::get('/address/{city}/{street}/{zipCode}',function(string $city, string $street, ?string $zipCode){
+    $zipCode = substr($zipCode,0,2)."-".substr($zipCode,2,3);
+    echo<<< Data
+    Kod Pocztowy : $zipCode <br>
+    Miasto : $city <br>
+    Ulica : $street
+    
+    <hr>
+    Data;
+});
+Route::get('/address1/{city?}/{street?}/{zipCode?}',function(string $city = "-", string $street = "-", int $zipCode = null){
+    $zipCode = is_null($zipCode) ? "brak danych" : $zipCode = substr($zipCode,0,2)."-".substr($zipCode,2,3);
+    //  ^
+    //  | to jest to samo
+    //  |
+    //  V
+    /*if(is_null($zipCode)){
+        $zipCode = "Brak Danych";
+    }else{
+        $zipCode = substr($zipCode,0,2)."-".substr($zipCode,2,3);
+    }
+    */
+   
+    echo<<< Data
+    Kod Pocztowy : $zipCode <br>
+    Miasto : $city <br>
+    Ulica : $street
+    
+    <hr>
+    Data;
+});
+Route::get('/address1/{city?}/{street?}/{zipCode?}',function(string $city = "-", string $street = "-", int $zipCode = null){
+    $zipCode = is_null($zipCode) ? "brak danych" : $zipCode = substr($zipCode,0,2)."-".substr($zipCode,2,3);
+    //  ^
+    //  | to jest to samo
+    //  |
+    //  V
+    /*if(is_null($zipCode)){
+        $zipCode = "Brak Danych";
+    }else{
+        $zipCode = substr($zipCode,0,2)."-".substr($zipCode,2,3);
+    }
+    */
+   
+    echo<<< Data
+    Kod Pocztowy : $zipCode <br>
+    Miasto : $city <br>
+    Ulica : $street
+    
+    <hr>
+    Data;
+})->name('address');
+Route::redirect('adres/{city}',"/address/{city}");
+
